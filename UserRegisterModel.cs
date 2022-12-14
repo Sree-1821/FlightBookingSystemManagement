ae9217527fc8fa1a80a64ebb0cd90c0ac6ab171e
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2022-12-14 05:54:23 +0000

    Add files via upload

---
+++ UserRegisterModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Web_API.Controllers;
+using Web_API.Models;
+
+namespace Web_API.Models
+{
+    public class UserRegisterModel
+    {
+        [Required(ErrorMessage = "*")]
+        public int CustomerID { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public string Firstname { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public string Lastname { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public int Age { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public string Gender { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public string EmailID { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public string MobileNumber { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public string Username { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public string Password { get; set; }
+    }
+}
+  
+    
